24d6cb2cda7ade7e0b5bad780a1e7c12ce96d607
Elenatr07 <e.turkina2010@yandex.ru>
2025-07-26 20:06:49 -0400

    anime

--- scripts/anime.js
+++ scripts/anime.js
@@ -25,8 +25,43 @@ $(window).scroll(function(){
 });
 $(document).ready(function(){
     scroling();
+
+    var imgs= [
+        "img/a1.png",
+        "img/a2.png",
+        "img/a3.png",
+        "img/a4.png",
+        "img/a5.png",
+        "img/a6.png",
+        "img/a7.png",
+        "img/a8.png",
+        "img/a8_1.png",
+        "img/a8_2.png",
+        "img/a8_1.png",
+        "img/a8_2.png",
+        "img/a8_3.png",
+        "img/a9.png",
+        "img/a10.png",
+        "img/a11.png"
+
+    ]
+    counter = 0;
+    setInterval(function() {
+        if ($("#dropbtn").hasClass("rus")){
+            $('#anime_main_rus').attr('src', '../'+ imgs[counter++]);
+        } else (
+          $('#anime_main').attr('src', imgs[counter++]) 
+        );   
+    
+    if(counter == imgs.length) {
+        counter = 0;
+    }
+}, 380);
     
 })
+    
+
+
 
 
 
